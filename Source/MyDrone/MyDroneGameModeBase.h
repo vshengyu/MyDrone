1ce854278b6926ebbd85f0db9d797e5d98acc7bc
vshengyu <346293109@qq.com>
2020-09-08 17:43:53 +0800

    fistr

---
+++ Source/MyDrone/MyDroneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyDroneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYDRONE_API AMyDroneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
